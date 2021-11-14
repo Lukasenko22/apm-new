84da32490ace94d30b8144290eb30c67fd148d62
Lukas <molcan.lukas@gmail.com>
2021-11-14 19:28:30 +0100

    Routing and product detail + TODO: HomeWork: display product in product detail

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,41 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {IProduct} from "./product";
+import {ProductService} from "./product.service";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle: string = "Product List";
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
   private _listFilter: string = "";
+
   filteredProducts: IProduct[] = [];
-  products: IProduct[] = [{
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2021",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    }
-  ];
+  products: IProduct[] = [];
+
+  errorMessage: string = '';
+  sub!: Subscription;
+
+  constructor(private productService: ProductService) {
+  }
+
   ngOnInit(): void {
-    this.listFilter = '';
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products
+        this.filteredProducts = this.products;
+      },
+      error: errorMsg => this.errorMessage = errorMsg
+    });
   }
 
   get listFilter(): string {
@@ -59,4 +54,9 @@ export class ProductListComponent implements OnInit {
   onRatingClicked(message: string): void {
     this.pageTitle = 'Product list: ' + message;
   }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
 }
